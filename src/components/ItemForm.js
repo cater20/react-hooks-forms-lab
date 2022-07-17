db89ea0ffb944dfd016e9f585b069c94bfb87bb0
chrisphine <onyangoketta@gmail.com>
2022-07-17 20:07:41 +0300

    function handlesubmit

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -4,7 +4,23 @@ import { v4 as uuid } from "uuid";
 function ItemForm(props) {
   const [name, setName] = useState("");
   const [ category, setCategory ] = useState("Produce");
-  
+
+
+  function nameChange(event) {
+    setName(event.target.value);
+  }
+
+  function categoryChange(event) {
+    setCategory(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    onItemFormSubmit({
+      id: uuid(),
+      name,
+      category,
+    });
+  }
   return (
     <form className="NewItem">
       <label>
